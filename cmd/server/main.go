7013f4981c2a880e49a665034ddfc675f1874642
Serhat ÖCAL <serhatocal1@gmail.com>
2025-03-12 21:46:19 +0300

    refactor

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -22,7 +22,7 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-    pgConnStr := "host=localhost user=almadansorgula password=15825270Se dbname=urlshortener sslmode=disable"
+    pgConnStr := "postgresql://almadansorgula:15825270Se@localhost:5432/urlshortener?sslmode=disable"
 
 	// Redis bağlantı bilgileri
 	redisAddr := fmt.Sprintf("%s:%s",
